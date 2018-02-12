864eac932c87d160129092fb89194cec27282bd2
patcurry <currypat1985@gmail.com>
2018-02-12 11:39:43 +0100

    first commit, all the git ignore stuff for node, python, emacs, and elastic beanstalk is here... should there be a docker section?

---
+++ app.js
@@ -0,0 +1,18 @@
+// get dependencies
+var express = require('express');
+var morgan = require('morgan');
+
+var app = express();
+var server = require('http').Server(app);
+port = process.env.PORT || 8080;
+
+// catch all other routes and return the index file
+app.get('/', (req, res) => {
+    res.send("Howdy");
+});
+
+// use morgan to log requests to the console
+app.use(morgan('dev'));
+
+server.listen(port);
+console.log('App running at http://localhost:' + port);
