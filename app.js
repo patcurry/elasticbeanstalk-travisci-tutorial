3c03bb9e94618c52c9dd5c22a1635bdf494401a9
patcurry <currypat1985@gmail.com>
2018-02-12 14:54:59 +0100

    changed Howdy to Howdy partner

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ port = process.env.PORT || 8080;
 
 // catch all other routes and return the index file
 app.get('/', (req, res) => {
-    res.send("Howdy");
+    res.send("Howdy partner.");
 });
 
 // use morgan to log requests to the console
